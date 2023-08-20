87a251fcdaa6ba30021344986faf4764c24f3a75
Fedor <lfa1@yandex.ru>
2023-08-20 13:31:41 +0300

    Update app_ui.h

--- src/include/app_ui.h
+++ src/include/app_ui.h
@@ -77,31 +77,10 @@ typedef struct {
     u32 released_time;
 } button_t;
 
-/* must be no more than FLASH_PAGE_SIZE (256) bytes */
-typedef struct __attribute__((packed)) {
-    u32 id;                     /* ID - ID_CONFIG                   */
-    u8  new_ota;                /* new ota flag                     */
-    u32 top;                    /* 0x0 .. 0xFFFFFFFF                */
-    u32 flash_addr_start;       /* flash page address start         */
-    u32 flash_addr_end;         /* flash page address end           */
-    u16 measurement_period;     /* measurement period in sec.       */
-    u8  device_model;           /* manufacturer of electric meters  */
-    u32 device_address;         /* see address on dislpay ID-20109  */
-    u32 tariff_multiplier;
-    u32 tariff_devisor;
-    u8  summation_formatting;
-    u16 voltage_multiplier;
-    u16 voltage_divisor;
-    u16 current_multiplier;
-    u16 current_divisor;
-    u16 crc;
-} em_config_t;
-
 /**********************************************************************
  * GLOBAL VARIABLES
  */
 
-extern em_config_t em_config;
 extern u8 device_online;
 extern u8 resp_time;
 
@@ -126,12 +105,8 @@ void init_button();
 void button_handler();
 u8 button_idle();
 
-void init_config(u8 print);
-void write_config();
-
 s32 getTimeCb(void *arg);
 s32 getTemperatureCb(void *arg);
-s32 measure_meterCb(void *arg);
 
 #if defined(MCU_CORE_8258)
 void adc_temp_init();
