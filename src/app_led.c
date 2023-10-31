083533e7a56326c36a7e3cc25c379866774c8bde
Fedor <lfa1@yandex.ru>
2023-10-31 23:55:57 +0300

    Update app_led.c

--- src/app_led.c
+++ src/app_led.c
@@ -11,13 +11,13 @@
 int32_t flashLedStatusCb(void *arg) {
     if (zb_isDeviceJoinedNwk() && device_online) {
         light_blink_stop();
-        if (fault_measure_flag) {
+        if (fault_measure_flag || dev_config.device_model == DEVICE_UNDEFINED) {
             light_blink_start(2, 30, 200);
         } else {
             light_blink_start(1, 30, 30);
         }
     } else {
-        if (fault_measure_flag) {
+        if (fault_measure_flag || dev_config.device_model == DEVICE_UNDEFINED) {
             light_blink_start(4, 30, 200);
         } else {
             light_blink_start(3, 30, 200);
