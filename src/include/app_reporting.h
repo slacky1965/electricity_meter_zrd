46aa4f90136b735070c80839586f0b17e8946519
Fedor <lfa1@yandex.ru>
2023-11-04 18:52:52 +0300

    Update app_reporting.h

--- src/include/app_reporting.h
+++ src/include/app_reporting.h
@@ -5,13 +5,12 @@ typedef struct {
     ev_timer_event_t *timerReportMinEvt;
     ev_timer_event_t *timerReportMaxEvt;
     reportCfgInfo_t  *pEntry;
-    uint32_t               time_posted;
+    uint32_t          time_posted;
 } app_reporting_t;
 
 extern app_reporting_t app_reporting[ZCL_REPORTING_TABLE_NUM];
 
-void app_reportAttrTimerStart();
-void app_reportNoMinLimit(void);
+void app_forcedReport(uint8_t endpoint, uint16_t claster_id, uint16_t attr_id);
 void app_reporting_init();
 void report_handler(void);
 
