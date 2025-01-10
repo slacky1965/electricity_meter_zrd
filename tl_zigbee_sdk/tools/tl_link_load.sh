1b7920c5177b6e52c959ba33ab03ca0bd3b639e6
Fedor <lfa1@yandex.ru>
2025-01-10 09:27:10 +0300

    delete old SDK

--- tl_zigbee_sdk/tools/tl_link_load.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-echo "*****************************************************"
-echo "this is post build!! current configure is :$1"
-rm -rf $2
-cp -f $1 $2
-echo "**************** end of post build ******************"
